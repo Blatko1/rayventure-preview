2f24a63fe633d71541db3e504635c8eac067aa9d
Blatko1 <kings.of.funny.gemplays@gmail.com>
2025-04-27 14:21:19 +0200

    removed the .gitgignore, now really working

---
+++ builds/v1/pkg/simple_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run: () => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hfe23a3c7c00b1377: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h92f67e728fd21635: (a: number, b: number) => void;
+export const closure1189_externref_shim: (a: number, b: number, c: any) => void;
+export const closure1194_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure1900_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
